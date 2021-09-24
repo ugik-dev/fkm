c44b227e03874184d786e7db0efc0187b83fdf6a
ugik <ugik.dev@gmail.com>
2021-09-25 00:57:06 +0700

    push

--- application/views/public/seminar/Page.php
+++ application/views/public/seminar/Page.php
@@ -2,31 +2,34 @@
 // google_translate("Ini bahasa indonesia menjadi..?",'en',"id","text")
 // google_translate('Bored', 'en', 'in' , 'text');
 ?>
+
 <body class="page-template-default page page-id-31 page-child parent-pageid-27 wp-custom-logo">
-  <section style="height: 30px;" id="page-header" class="page-header parallax-window section-row hidden-sm hidden-xs" data-parallax="scroll" data-image-src="<?=base_url()?>assets/img/logo-unsri.png">
+  <section style="height: 30px;" id="page-header" class="page-header parallax-window section-row hidden-sm hidden-xs" data-parallax="scroll" data-image-src="<?= base_url() ?>assets/img/logo-unsri.png">
     <div class="container">
       <div class="page-header-title">
-      <?php if ($_COOKIE['lang_set'] == 'in') { ?>
-        <h1 class="entry-title"><a href="<?= base_url() ?>" title="<?=$contentData['berita_judul']?>"><?=$contentData['berita_judul']?></a></h1>
-      <?php }else{ ?>
-        <h1 class="entry-title"><a href="<?= base_url() ?>" title="<?=$contentData['berita_judul_en']?>"><?=$contentData['berita_judul_en']?></a></h1>  
+        <?php if ($_COOKIE['lang_set'] == 'in') { ?>
+          <h1 class="entry-title"><a href="<?= base_url() ?>" title="<?= $contentData['berita_judul'] ?>"><?= $contentData['berita_judul'] ?></a></h1>
+        <?php } else { ?>
+          <h1 class="entry-title"><a href="<?= base_url() ?>" title="<?= $contentData['berita_judul_en'] ?>"><?= $contentData['berita_judul_en'] ?></a></h1>
         <?php }; ?>
 
         <?php if ($_COOKIE['lang_set'] == 'in') { ?>
-            <div class="breadcrumb"> <span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="Fakultas Ilmu Kesehatan - Universitas Sriwijaya." href="<?= base_url() ?>" class="home"><span property="name">Fakultas Ilmu Kesehatan</span></a>
-            <meta property="position" content="1"></span>&nbsp;&nbsp;/&nbsp;&nbsp;<span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="<?= base_url() . '' ?>" href="<?= base_url() . '' ?>" class="post post-page"><span property="name"> Seminar </span></a>
+          <div class="breadcrumb"> <span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="Fakultas Ilmu Kesehatan - Universitas Sriwijaya." href="<?= base_url() ?>" class="home"><span property="name">Fakultas Ilmu Kesehatan</span></a>
+              <meta property="position" content="1">
+            </span>&nbsp;&nbsp;/&nbsp;&nbsp;<span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="<?= base_url() . '' ?>" href="<?= base_url() . '' ?>" class="post post-page"><span property="name"> Seminar </span></a>
 
-     
-            <?php }else{ ?>
-     
-                <div class="breadcrumb"> <span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="Go to Public Healty Faculty - Sriwijaya University." href="<?= base_url() ?>" class="home"><span property="name">Public Healty Faculty</span></a>
-             <meta property="position" content="1"></span>&nbsp;&nbsp;/&nbsp;&nbsp;<span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="<?= base_url() . '' ?>" href="<?= base_url() . '' ?>" class="post post-page"><span property="name"> Qolloquium</span></a>
-     
+
+            <?php } else { ?>
+
+              <div class="breadcrumb"> <span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="Go to Public Healty Faculty - Sriwijaya University." href="<?= base_url() ?>" class="home"><span property="name">Public Healty Faculty</span></a>
+                  <meta property="position" content="1">
+                </span>&nbsp;&nbsp;/&nbsp;&nbsp;<span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="<?= base_url() . '' ?>" href="<?= base_url() . '' ?>" class="post post-page"><span property="name"> Qolloquium</span></a>
+
                 <?php }; ?>
-     
 
-        </div>
-      </div>
+
+              </div>
+          </div>
   </section>
   <section class="page-row article-row bg-white">
     <div class="container">
@@ -34,37 +37,37 @@
         <div class="col-md-8 col-sm-8 col-xs-12">
 
           <div id="post-31" class="article-frame post-31 page type-page status-publish hentry">
-         
-          <?php if ($_COOKIE['lang_set'] == 'in') { ?>
-            <h1 class="entry-title  hidden-lg hidden-md"><a href="" title="<?=$contentData['berita_judul']?>"><?=$contentData['berita_judul']?></a></h1>
-            <div class="entry-content" id='news-list'>
-            <?=$contentData['berita_isi']?>
-     
-            <?php }else{ ?>
-                <h1 class="entry-title  hidden-lg hidden-md"><a href="" title="<?=$contentData['berita_judul_en']?>"><?=$contentData['berita_judul_en']?></a></h1>
-            <div class="entry-content" id='news-list'>
-            <?=$contentData['berita_isi_en']?>
+
+            <?php if ($_COOKIE['lang_set'] == 'in') { ?>
+              <h1 class="entry-title  hidden-lg hidden-md"><a href="" title="<?= $contentData['berita_judul'] ?>"><?= $contentData['berita_judul'] ?></a></h1>
+              <div class="entry-content" id='news-list'>
+                <?= $contentData['berita_isi'] ?>
+
+              <?php } else { ?>
+                <h1 class="entry-title  hidden-lg hidden-md"><a href="" title="<?= $contentData['berita_judul_en'] ?>"><?= $contentData['berita_judul_en'] ?></a></h1>
+                <div class="entry-content" id='news-list'>
+                  <?= $contentData['berita_isi_en'] ?>
                 <?php }; ?>
 
-        
-            </div>
-            <div class="col-md-7">
-              <div class="text-center">
-                <ul class="pagination " style="visibility: visible; ">
-                  <div class="pagging text-center">
-                    <nav>
-                      <ul class="pagination justify-content-center" id='pagination'>
-                      </ul>
-                    </nav>
+
+                </div>
+                <div class="col-md-7">
+                  <div class="text-center">
+                    <ul class="pagination " style="visibility: visible; ">
+                      <div class="pagging text-center">
+                        <nav>
+                          <ul class="pagination justify-content-center" id='pagination'>
+                          </ul>
+                        </nav>
+                      </div>
+                    </ul>
                   </div>
-                </ul>
+                </div>
               </div>
-            </div>
           </div>
+          <?php $this->load->view('public/fragment/slidebarSeminar') ?>
         </div>
-        <?php $this->load->view('public/fragment/slidebarSeminar') ?>
       </div>
-    </div>
   </section>
   <style>
     .page-header {
@@ -76,6 +79,6 @@
 
 <script type='text/javascript'>
   $(document).ready(function() {
-  
+
   });
 </script>
